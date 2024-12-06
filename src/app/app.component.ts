3999e1dbc4c7ae96d13f8105668fb5288fc37d4b
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2024-12-05 23:55:34 -0500

    ConfigCarrusel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'mi-app';
   isMenuOpen = false; // Estado para el menú móvil
   isSubmenuOpen = false; // Estado para el submenú "Conta"
+
+  // Array con las imágenes
+  images = [
+    {
+      src: '/assets/carrusel/imagen1.jpg'
+    },
+    {
+      src: '/assets/carrusel/imagen4.jpg'
+    },
+    {
+      src: '/assets/carrusel/imagen3.jpg'
+    }
+  ];
+  currentIndex: number = 0;
+  interval: any;
+
+  // Se ejecuta cuando el componente se inicializa
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    this.interval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 6000); // Cambia de imagen cada 5 segundos
+  }
+
+  goToNext() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  goToPrevious() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
@@ -17,5 +58,5 @@ export class AppComponent {
     this.isSubmenuOpen = !this.isSubmenuOpen;
   }
 
- 
+
 }
