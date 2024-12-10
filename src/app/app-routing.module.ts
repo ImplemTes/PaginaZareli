4acd2d7ce1fb3e173eed148b9b9ee6d3c3e9606c
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2024-12-09 20:32:36 -0500

    PlantillaLista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },  // Ruta para la página de inicio
-  { path: 'contacto', component: ContactoComponent }  // Ruta para la página de contacto
+  {
+    path: '',
+    redirectTo: 'home',  // Redirige a 'home' cuando la ruta esté vacía
+    pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent },  // Ruta para la página de inicio
+  { path: 'contacto', component: ContactoComponent },  // Ruta para la página de contacto
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })  // Precarga de módulos (útil si agregas lazy loading)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  isMenuOpen = false;  
+  isSubmenuOpen = false; 
+  images = [
+    {
+      src: '/assets/carrusel/imagen1.jpg'
+    },
+    {
+      src: '/assets/carrusel/imagen4.jpg'
+    },
+    {
+      src: '/assets/carrusel/imagen3.jpg'
+    }
+  ];
+  currentIndex: number = 0;
+  interval: any;
 
+ 
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    this.interval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 6000);  
+  }
+
+  goToNext() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  goToPrevious() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleSubmenu() {
+    this.isSubmenuOpen = !this.isSubmenuOpen;
+  }
+
+  values = [
+    {
+      title: 'Puntualidad',
+      description:
+        'Nos comprometemos a entregar los materiales de construcción a tiempo, garantizando la satisfacción de nuestros clientes.',
+    },
+    {
+      title: 'Innovación',
+      description:
+        'Incorporamos las últimas tecnologías para optimizar nuestros procesos y mejorar la calidad de nuestros productos.',
+    },
+    {
+      title: 'Calidad',
+      description:
+        'Ofrecemos materiales de la más alta calidad, asegurando durabilidad y fiabilidad en todos nuestros productos.',
+    },
+    {
+      title: 'Seguridad',
+      description:
+        'La seguridad es nuestra prioridad, asegurando un entorno de trabajo seguro tanto para nuestros empleados como para nuestros clientes.',
+    },
+    {
+      title: 'Responsabilidad',
+      description:
+        'Nos comprometemos con la ética y la responsabilidad en cada proyecto, respetando el medio ambiente y cumpliendo nuestras promesas con los clientes.',
+    },
+        {
+      title: 'Compromiso',
+      description:
+        'Compromiso con nuestros clientes, empleados y la sociedad. Nos dedicamos a cumplir nuestras promesas y garantizar que cada proyecto sea realizado con la máxima dedicación.',
+    },
+  ];
+
+  openedIndex: number | null = null;
+
+  toggleAccordion(index: number) {
+    // Si el índice está abierto, lo cerramos, sino lo abrimos
+    this.openedIndex = this.openedIndex === index ? null : index;
+  }
 }
