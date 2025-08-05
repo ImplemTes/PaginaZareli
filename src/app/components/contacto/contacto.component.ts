de71a1d1fa582e6f5abad87178c4ef25a1d83673
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2025-08-05 10:52:08 -0500

    ConfigCompbnt

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -2,90 +2,11 @@ import { Component } from '@angular/core';
 import { DocumentoService } from '../../services/documento.service';
 import Swal from 'sweetalert2';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { Documento } from '../../models/documento';
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.scss']
 })
 export class ContactoComponent {
-  // Propiedades enlazadas al formulario
-  documentoForm: FormGroup;
 
-
-  seConsulto: boolean = false; // se activa cuando se hace una búsqueda
-  isLoading: boolean = false;
-  resultados: Documento[] = [];
-  // Constructor con el servicio inyectado
-  constructor(
-    private consultaService: DocumentoService,
-    private fb: FormBuilder
-  ) {
-
-    this.documentoForm = this.fb.group({
-      ruc: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
-      fechaDesde: ['', [Validators.required]],
-      fechaHasta: ['', [Validators.required]],
-      tipoComprobante: ['', [Validators.required]],
-      serie: ['', [Validators.required]]
-    });
-  }
-
-  // Método para enviar la consulta
-
-  EnviarConsulta() {
-    if (this.documentoForm.valid) {
-      this.isLoading = true; // Activa el loader
-      // Puedes activar un loader aquí
-
-      this.consultaService.sendInfo(this.documentoForm.value).subscribe({
-        next: (data: Documento[]) => {
-          this.isLoading = false; // Desactiva el loader en caso de éxito
-          this.resultados = data; // Guarda los resultados recibidos
-          this.seConsulto = true; // 👈 aquí se activa
-          if (data.length > 0) {
-            this.showAlert('Consulta enviada', 'Tu solicitud fue procesada correctamente', 'success');
-          } else {
-            this.showAlert('Sin resultados', 'No se encontraron comprobantes con los datos ingresados.', 'info');
-          }
-        },
-        error: (error) => {
-          this.isLoading = false;
-          this.seConsulto = false;
-          this.showAlert('Error al enviar la consulta', 'Por favor, intenta nuevamente', 'error');
-        }
-      });
-    } else {
-      console.error('Formulario inválido');
-      this.documentoForm.markAllAsTouched(); // Opcional: muestra errores al usuario
-    }
-  }
-
-  // Método para limpiar el formulario
-
-  LimpiarForm(): void {
-    this.isLoading = false;
-    this.seConsulto = false; // 👈 para que no se muestre el mensaje
-    this.resultados = []; // 👈 Vacía la lista de resultados
-    this.documentoForm.reset({
-      ruc: '',
-      fechaDesde: '',
-      fechaHasta: '',
-      tipoComprobante: '',
-      serie: ''
-    });
-  }
-
-
-  // Método para mostrar la alerta SweetAlert2
-  showAlert(title: string, text: string, icon: 'success' | 'error' | 'info') {
-    Swal.fire({
-      title: title,
-      text: text,
-      icon: icon,
-      confirmButtonText: 'Aceptar'
-    });
-  }
-
-  //npm install sweetalert2 @sweetalert2/ngx-sweetalert2
 }
--- src/app/services/documento.service.ts
+++ src/app/services/documento.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Documento } from '../models/documento';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +10,5 @@ export class DocumentoService {
   private apiUrl = environment.apiUrl + '/consuldoc';
 
   constructor(private http: HttpClient) {}
-  sendInfo(data: any): Observable<Documento[]> {
-    return this.http.post<Documento[]>(this.apiUrl, data);
-  }
+ 
 }
